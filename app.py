3a620b5b3f35819ad7ccd2b6ad9412c546d4f8a6
allurim <aulim.id@gmail.com>
2023-01-29 10:40:45 +0700

    Rearrange codes; Remove unused lines; Add session state variables for sorting

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ Data didapatkan dari [laman harga notebook Viraindo](http://viraindo.com/noteboo
 Data diupdate harian.
 ''')
 
+#region Functions
 def show_dataframe_paginated(df, page: int, show_row: int):
     start_idx = (page - 1) * show_row
     end_idx = (page * show_row)
@@ -52,68 +53,36 @@ def get_data(date: str):
     df.drop(columns=['date'], inplace=True)
     df['price'] = pd.to_numeric(df['price'], downcast='float')
     return df
+#endregion
+
+#region Init Session State Variables
+if 'page_current' not in st.session_state:
+    st.session_state.page_current = 1
+
+if 'filter_params' not in st.session_state:
+    st.session_state.filter_params = []
+
+if 'min_price' not in st.session_state:
+    st.session_state.min_price = 0.0
+
+if 'max_price' not in st.session_state:
+    st.session_state.max_price = 9999999999.0
+
+if 'sorting' not in st.session_state:
+    st.session_state.sorting = False
+
+if 'sort_by' not in st.session_state:
+    st.session_state.sort_by = 'name'
+#endregion
 
 current_date = get_today_date()
 df = get_data(current_date)
 
-if 'page_current' not in st.session_state:
-    st.session_state.page_current = 1
-
-if 'filter_params' in st.session_state and len(st.session_state.filter_params) > 0:
-    df = filter_name(df, st.session_state.filter_params)
-    filter_txts, pop_filter, clear_filter = st.columns([18,3,3])
-    filter_txts.write(f"Anda mencari {', '.join(st.session_state.filter_params)}")
-    # pop_filter_button = pop_filter.button(
-    #     "Hapus terbaru", 
-    #     disabled=len(st.session_state.filter_params) < 1)
-    # clear_filter_button = clear_filter.button(
-    #     "Hapus semua"
-    # )
-
-    # if pop_filter_button:
-    #     st.session_state.filter_params.pop()
-    #     st.experimental_rerun()
-
-    # if clear_filter_button:
-    #     st.session_state.filter_params.clear()
-    #     st.session_state.min_price = 0.0
-    #     st.session_state.max_price = 9999999999.0
-    #     st.experimental_rerun()
-else:
-    st.session_state.filter_params = []
-
-if 'min_price' in st.session_state and 'max_price' in st.session_state:
-    df = filter_price(df, st.session_state.min_price, st.session_state.max_price)
-    st.write(f'Mencari produk dengan harga antara {st.session_state.min_price} sampai {st.session_state.max_price}')
-else:
-    st.session_state.min_price = 0.0
-    st.session_state.max_price = 9999999999.0
-    df = filter_price(df, st.session_state.min_price, st.session_state.max_price)
-    st.write(f'Mencari produk dengan harga antara {st.session_state.min_price} sampai {st.session_state.max_price}')
-
-# with st.form("Filter form", True):
-#     filter = st.text_input("Cari produk yang mengandung kata:")
-#     min, max = st.columns([1,1])
-#     #TODO: Fix the max price
-#     min_val = st.session_state.min_price if st.session_state.min_price is not None else 0.0
-#     max_val = st.session_state.max_price if st.session_state.max_price is not None else 9999999999.0
-#     min_price = min.number_input("Harga minimum", min_value=0.0, max_value=9999999999.0, value=float(min_val))
-#     max_price = max.number_input("Harga maksimum", min_value=0.0, max_value=9999999999.0, value=float(max_val))
-#     filtered = st.form_submit_button("Cari")
-
-#     if filtered:
-#         if filter:
-#             st.session_state.filter_params.append(filter)
-#         st.session_state.min_price = min_price
-#         st.session_state.max_price = max_price
-#         st.session_state.page_current = 1
-#         st.experimental_rerun()
-
 with st.expander("Lakukan pencarian", expanded=True):
     filter = stt.st_tags(label="Cari produk dengan kata kunci:", text="Tekan enter untuk menambah kata kunci", key="search_query", value=st.session_state.filter_params)
     min, max = st.columns([1,1])
-    min_val = st.session_state.min_price if st.session_state.min_price is not None else 0.0
-    max_val = st.session_state.max_price if st.session_state.max_price is not None else 9999999999.0
+    min_val = st.session_state.min_price
+    max_val = st.session_state.max_price
     min_price = min.number_input("Harga minimum", min_value=0.0, max_value=9999999999.0, value=float(min_val))
     max_price = max.number_input("Harga maksimum", min_value=0.0, max_value=9999999999.0, value=float(max_val))
     filtered = st.button("Cari")
@@ -132,9 +101,17 @@ with st.expander("Lakukan pencarian", expanded=True):
         st.session_state.page_current = 1
         st.experimental_rerun()
 
-
 show_rows = 20
 page_limit = math.ceil(len(df) / show_rows)
+
+if 'filter_params' in st.session_state and len(st.session_state.filter_params) > 0:
+    df = filter_name(df, st.session_state.filter_params)
+    filter_txts, pop_filter, clear_filter = st.columns([18,3,3])
+    filter_txts.write(f"Anda mencari {', '.join(st.session_state.filter_params)}")
+
+if st.session_state.min_price > 0 or st.session_state.max_price < 9999999999.0:
+    df = filter_price(df, st.session_state.min_price, st.session_state.max_price)
+    st.write(f'Mencari produk dengan harga antara {st.session_state.min_price} sampai {st.session_state.max_price}')
 
 if len(df) > 0:
     curr_page_start_product = ((st.session_state.page_current - 1) * show_rows) + 1
@@ -143,10 +120,10 @@ if len(df) > 0:
     else:
         curr_page_end_product = len(df)
     st.write(f"Ditemukan {len(df)} produk. Tampilkan produk ke-{curr_page_start_product} sampai {curr_page_end_product}")
-    prev, _, next = st.columns([4,30,4])
+    prev_top, _, next_top = st.columns([4,30,4])
 
-    prev_button = prev.button("Prev", disabled=st.session_state.page_current <= 1, key='prev_top')
-    next_button = next.button("Next", disabled=st.session_state.page_current >= page_limit, key='next_top')
+    prev_button_top = prev_top.button("Prev", disabled=st.session_state.page_current <= 1, key='prev_top')
+    next_button_top = next_top.button("Next", disabled=st.session_state.page_current >= page_limit, key='next_top')
 
     st.table(show_dataframe_paginated(df, st.session_state.page_current, show_rows))
 
@@ -154,11 +131,11 @@ if len(df) > 0:
     prev_button_bottom = prev_bottom.button("Prev", disabled=st.session_state.page_current <= 1, key='prev_bottom')
     next_button_bottom = next_bottom.button("Next", disabled=st.session_state.page_current >= page_limit, key='next_bottom')
 
-    if prev_button or prev_button_bottom:
+    if prev_button_top or prev_button_bottom:
         st.session_state.page_current-=1
         st.experimental_rerun()
         
-    if next_button or next_button_bottom:
+    if next_button_top or next_button_bottom:
         st.session_state.page_current+=1
         st.experimental_rerun()
 
